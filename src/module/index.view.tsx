82b9fefcb39bcf8b71c247ae29f1152c8a68a196
kuiwu <danwu.wdw@alibaba-inc.com>
2023-02-28 10:51:05 +0800

    chore(deps): update opensumi packages to v2.22.5

--- src/module/index.view.tsx
+++ src/module/index.view.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Button, message } from '@opensumi/ide-components';
 
 export const TestView = (props: {name: string}) => {
-  return <div>
+  return <div style={{ margin: 10 }}>
     <Button onClick={() => message.success('hello!')}>Hello {props.name}</Button>
   </div>
 };
